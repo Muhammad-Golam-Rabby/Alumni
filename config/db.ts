c598d445a5841020714a87ab78c4f9a358c7c7f0
MAMUNdevBD <mamundevbd@gmail.com>
2024-12-26 21:41:50 +0600

    git secret fix

--- config/db.ts
+++ config/db.ts
@@ -1,7 +1,7 @@
 import { apps } from "firebase-admin";
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "./firebaseService.json";
+import serviceAccount from "./firebaseServices.json";
 if (!apps.length) {
   initializeApp({
     credential: cert(serviceAccount as any),
